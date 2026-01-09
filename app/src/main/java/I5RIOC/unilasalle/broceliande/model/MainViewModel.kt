c6c7a143653d66952b019d9440eba486de901985
etil2jz <81570777+etil2jz@users.noreply.github.com>
2026-01-09 14:21:15 +0100

    logique backend du panier

--- app/src/main/java/I5RIOC/unilasalle/broceliande/model/MainViewModel.kt
+++ app/src/main/java/I5RIOC/unilasalle/broceliande/model/MainViewModel.kt
@@ -1,19 +1,37 @@
 package I5RIOC.unilasalle.broceliande.model
 
+import I5RIOC.unilasalle.broceliande.data.BroceliandeDatabase
 import I5RIOC.unilasalle.broceliande.network.RetrofitInstance
+import android.app.Application
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+	// liste produits
 	var productList = mutableStateOf<List<Product>>(emptyList())
 		private set
 
+	// DAO panier
+	private val dao = BroceliandeDatabase.getDatabase(application).cartDao()
+
+	// refresh UI automatique
+	private val _cartItems = MutableStateFlow<List<CartItem>>(emptyList())
+	val cartItems = _cartItems.asStateFlow()
+
 	init {
 		fetchProducts()
+		viewModelScope.launch {
+			dao.getCartItems().collect { items ->
+				_cartItems.value = items
+			}
+		}
 	}
 
+	// récupérer les produits avec l'API
 	private fun fetchProducts() {
 		viewModelScope.launch {
 			try {
@@ -21,6 +39,33 @@ class MainViewModel : ViewModel() {
 			} catch (e: Exception) {
 				e.printStackTrace()
 			}
+		}
+	}
+
+	// ajout d'un produit au panier
+	fun addToCart(product: Product) {
+		viewModelScope.launch {
+			val existingItem = dao.getCartItemById(product.id)
+			if (existingItem != null) {
+				val updatedItem = existingItem.copy(quantity = existingItem.quantity + 1)
+				dao.insertOrUpdate(updatedItem)
+			} else {
+				val newItem = CartItem(
+					id = product.id,
+					title = product.title,
+					price = product.price,
+					image = product.image,
+					quantity = 1
+				)
+				dao.insertOrUpdate(newItem)
+			}
+		}
+	}
+
+	// suppression d'un produit au panier
+	fun removeFromCart(product: Product) {
+		viewModelScope.launch {
+			dao.deleteItem(product.id)
 		}
 	}
 }
